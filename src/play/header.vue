687ef2f0d510ccfd59367d63a069c9c58daa063b
noordinary <1556919050@qq.com>
2017-09-11 17:16:31 +0800

    sadas

---
+++ src/play/header.vue
@@ -0,0 +1,11 @@
+<template>
+    <header>
+        <a href="#"><img src="img/icon-menu.svg" alt=""></a>
+        <h1>Just Dance</h1>
+        <a href="#"><img src="img/icon-move.svg" alt=""></a>
+    </header>
+</template>
+
+<style>
+    
+</style>
