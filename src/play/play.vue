aa34bc1e60da43e3c4db6cdb312997b0e83a1618
noordinary <1556919050@qq.com>
2017-09-13 15:57:50 +0800

    播放

--- src/play/play.vue
+++ src/play/play.vue
@@ -2,7 +2,9 @@
 <template>
     <div>
         
-        <compo-header></compo-header>
+        <!-- <compo-header></compo-header> -->
+
+        <!-- <compo-content></compo-content> -->
 
         <compo-footer></compo-footer>
 
@@ -14,14 +16,32 @@
 
     import Header from './header.vue'
     import Footer from './footer.vue'
+    import Content from './content.vue'
 
     export default {
         components: {
             'compo-header': Header,
-            'compo-footer': Footer
+            'compo-footer': Footer,
+            'compo-content': Content
+
         }
     }
 </script>
+
+<style lang="scss">
+    .header,.footer{
+        display: none;
+    }
+</style>
+
+<style>
+    body{
+        font-family: "微软雅黑";
+    }
+    .router-link-exact-active{
+        display: none;
+    }
+</style>
 
 
 
