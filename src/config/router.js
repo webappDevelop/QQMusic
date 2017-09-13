0bf104e0bf453f85fbcca385852e9e12fed275f1
CZSakura <2858911477@qq.com>
2017-09-13 22:23:33 +0800

    Sakura

--- src/config/router.js
+++ src/config/router.js
@@ -3,6 +3,7 @@ import My from '../my/my.vue'
 import MusicHall from '../musicHall/musicHall.vue'
 import Find from '../find/find.vue'
 import Play from '../play/play.vue'
+import Ranking from '../musicHall/ranking/ranking.vue'
 
 export default {
     routes: [
@@ -25,6 +26,11 @@ export default {
             name: 'play',
             path: '/play',
             component: Play
+        },
+        {
+            name: 'ranking',
+            path: '/ranking',
+            component: Ranking
         }
     ]
 }
